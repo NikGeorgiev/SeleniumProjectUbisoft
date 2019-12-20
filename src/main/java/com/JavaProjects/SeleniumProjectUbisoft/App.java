d76804a1005ea9537fa40ffd9a36f270e1b9a1ac
Nikolay <ngeorgiev97@gmail.com>
2019-12-20 17:16:45 +0000

    initial commit

---
+++ src/main/java/com/JavaProjects/SeleniumProjectUbisoft/App.java
@@ -0,0 +1,13 @@
+package com.JavaProjects.SeleniumProjectUbisoft;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/JavaProjects/SeleniumProjectUbisoft/AppTest.java
@@ -0,0 +1,20 @@
+package com.JavaProjects.SeleniumProjectUbisoft;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
